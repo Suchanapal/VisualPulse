c70f3a9897db03f872db03d2f3b834cc95dd508f
Suchanapal <suchanapal22@gmail.com>
2023-06-28 10:36:24 +0600

    first commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,22 +1,25 @@
 import { g, auth, config } from '@grafbase/sdk'
 
-const User= g.model('User',{
-  name: g.string().length({min:2, max:15}),
-  wmail:g.string().unique(),
-  avatarUrl:g.url(),
-  description:g.string().optional(),
-  githuburl:g.url().optional(),
-  projects: g.relation(()=>Project).list().optional(),
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(), 
+  projects: g.relation(() => Project).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
 })
 
 const Project=g.model('Project',{
-  title:g.string().length({min:3}),
-  description:g.string(),
-  image:g.url(),
-  liveSiteUrl:g.url(),
-  githuburl:g.url(),
-  category:g.string().search(),
-  createdBy:g.relation(() => User),
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(),
+  liveSiteUrl: g.url(), 
+  githubUrl: g.url(), 
+  category: g.string().search(),
+  createdBy:g.relation(() => User)
 
 
 })
